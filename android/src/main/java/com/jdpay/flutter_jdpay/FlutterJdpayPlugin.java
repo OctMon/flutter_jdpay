ff305ddd96a39fb01ecaa59b122bab8fb37d1aea
OctMon <octmon@qq.com>
2019-08-29 12:17:19 +0800

    add sdk

--- android/src/main/java/com/jdpay/flutter_jdpay/FlutterJdpayPlugin.java
+++ android/src/main/java/com/jdpay/flutter_jdpay/FlutterJdpayPlugin.java
@@ -6,6 +6,9 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import com.jdpaysdk.author.Constants;
+import com.jdpaysdk.author.JDPayAuthor;
+
 /** FlutterJdpayPlugin */
 public class FlutterJdpayPlugin implements MethodCallHandler {
   /** Plugin registration. */
@@ -16,15 +19,20 @@ public class FlutterJdpayPlugin implements MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
+        JDPayAuthor jdPayAuthor = new JDPayAuthor();
     switch (call.method) {
       case "getVersion":
-        result.success("待实现");
+//        result.success(jdPayAuthor.getVersionCode(this));
         break;
       case "registerService":
-        //TODO:待实现
         break;
       case "pay":
-        //TODO:待实现
+        String appId = call.argument("appId");
+        String merchantId = call.argument("merchantId");
+        String orderId = call.argument("orderId");
+        String signData = call.argument("signData");
+//        jdPayAuthor.author(MainActivity.this, orderId, merchantId, appId, signData);
+        result.success(JDPayAuthor.JDPAY_RESULT);
         break;
       default:
         result.notImplemented();
